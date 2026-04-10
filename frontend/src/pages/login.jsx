ea45b3236a6fb495d5c95d6d7e1e3be66f3a09fb
Bhupendra Sharma <bhupendrasharma3423@gmail.com>
2026-04-10 22:46:10 +0530

    Updated login UI

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -6,14 +6,14 @@ Container,
 Paper,
 Typography
 } from "@mui/material";
-
 import { Link, useNavigate } from "react-router-dom";
 import api from "../api";
 import { toast } from "react-toastify";
+
 export default function Login({ setToken }) {
 const [data, setData] = useState({ email: "", password: "" });
+const navigate = useNavigate();
 
-const navigate = useNavigate(); 
 const login = async () => {
   if (!data.email || !data.password) {
     return toast.error("Please fill all fields");
@@ -21,15 +21,14 @@ const login = async () => {
 
   try {
     const res = await api.post("/auth/login", data);
+
     localStorage.setItem("token", res.data.token);
     localStorage.setItem("user", JSON.stringify(res.data.user));
 
     setToken(res.data.token);
 
-    toast.success("Login successful ");
-
-    navigate("/profile"); 
-
+    toast.success("Login successful");
+    navigate("/profile");
   } catch (err) {
     toast.error(err.response?.data?.msg || "Login failed");
   }
@@ -37,32 +36,56 @@ const login = async () => {
 
 return (
   <Container maxWidth="sm">
-    <Paper sx={{ p: 4, mt: 10 }}>
-      <Typography variant="h5">Login</Typography>
+    <Paper sx={{ p: 4, mt: 10, borderRadius: 4 }}>
+      
+      <Typography
+        variant="h4"
+        fontWeight="bold"
+        align="center"
+        sx={{ mb: 2, color: "#1976d2" }}
+      >
+        MiniGram 
+      </Typography>
+
+      <Typography variant="h5" align="center">
+        Login
+      </Typography>
 
       <TextField
         label="Email"
         fullWidth
         sx={{ mt: 2 }}
-        onChange={(e) => setData({ ...data, email: e.target.value })}
+        onChange={(e) =>
+          setData({ ...data, email: e.target.value })
+        }
       />
+
       <TextField
         label="Password"
         type="password"
         fullWidth
         sx={{ mt: 2 }}
-        onChange={(e) => setData({ ...data, password: e.target.value })}
+        onChange={(e) =>
+          setData({ ...data, password: e.target.value })
+        }
       />
 
-      <Button variant="contained" sx={{ mt: 2 }} onClick={login}>
+      <Button
+        variant="contained"
+        fullWidth
+        sx={{ mt: 3 }}
+        onClick={login}
+      >
         Login
       </Button>
-      <Typography sx={{ mt: 2 }}>
+
+      <Typography sx={{ mt: 2 }} align="center">
         Don't have an account?{" "}
         <Link to="/signup" style={{ color: "blue" }}>
           Signup
         </Link>
       </Typography>
+
     </Paper>
   </Container>
 );
