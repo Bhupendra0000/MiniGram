0f161b72ec46d34ae9ebe181d3eb96ac7779e175
Bhupendra Sharma <bhupendrasharma3423@gmail.com>
2026-04-10 16:38:21 +0530

    Added dotenv config

--- backend/backend/server.js
+++ backend/backend/server.js
@@ -3,21 +3,32 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const http = require("http");
 const { Server } = require("socket.io");
+require("dotenv").config();
+
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb+srv://nikcode00_db_user:Nik1o2o3o40@cluster1.zmlmrbh.mongodb.net/mydb")
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB connected"))
+.catch(err => console.log(err));
+
 const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: "*" } });
 
 app.set("io", io);
+
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/posts", require("./routes/post"));
 app.use("/uploads", express.static("uploads"));
 
-server.listen(5000, () => 
-console.log("Backend running on port 5000"));
+app.get("/", (req, res) => {
+res.send("Backend is running 🚀");
+});
 
+const PORT = process.env.PORT || 5000;
 
+server.listen(PORT, () => {
+console.log("Server running on " + PORT);
+});
 
