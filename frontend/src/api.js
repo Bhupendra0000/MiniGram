74e9f10fb424aa21af427b400af9192109b22aa6
Bhupendra Sharma <bhupendrasharma3423@gmail.com>
2026-04-10 16:54:00 +0530

    Updated API base URL for production

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 const api = axios.create({
-baseURL: "http://localhost:5000/api"
+  baseURL: `${import.meta.env.VITE_API_URL}/api`
 });
 
 api.interceptors.request.use((req) => {
-const token = localStorage.getItem("token");
-if (token) req.headers.authorization = token;
-return req;
+  const token = localStorage.getItem("token");
+  if (token) req.headers.authorization = token;
+  return req;
 });
 
 export default api;
